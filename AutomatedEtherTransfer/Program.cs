e3e4dc38a76cfb57c31e9b87d87857a34429f449
chaitanya potti <chaitanya.potti@gmail.com>
2018-07-31 15:33:15 +0530

    Final Ether sending

--- AutomatedEtherTransfer/Program.cs
+++ AutomatedEtherTransfer/Program.cs
@@ -8,9 +8,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nethereum.Util;
 
 namespace AutomatedEtherTransfer
 {
@@ -19,56 +19,52 @@ namespace AutomatedEtherTransfer
 
         static void Main(string[] args)
         {
+            //MainSend();
+            Task.Run(async () => await Send()).GetAwaiter().GetResult();
+
+        }
+
+        private static async Task Send()
+        {
             var account = SetupAccount();
             var recipients = ReadCsv();
+            //This account is for test purposes only. No real ether in it.
+            //var recipients = new Wallet("blue inherit drum enroll amused please camp false estate flash sell right", null).GetAddresses();
+            var transanctionHashes = new List<string>();
+            
             //var recipients = new List<Records>() { new Records() { Value = 10000000000000000, Address = "0x5CC494843e3f4AC175A5e730c300b011FAbF2cEa" } };
             foreach (var recipient in recipients)
             {
                 try
                 {
-                    var web3 = GetConnection();
-                    var receipt = SendEther(account, recipient, web3).Result;
+                    var web3 = GetConnection(account);
+                    //var balance = await web3.Eth.GetBalance.SendRequestAsync(recipients[0]);
+                    var transactionHash = await SendEther(account, recipient, web3);
                 }
                 catch (System.Exception)
                 {
-                    MessageBox.Show("Failed");                                        
+                    MessageBox.Show("Failed");
                 }
-                Thread.Sleep(30000);
             }
         }
 
-        private static async Task<TransactionReceipt> SendEther(Account account, Records recipient, Web3 web3)
+        private static Task<string> SendEther(Account account, Records recipient, Web3 web3)
         {
-            //web3.Eth.
-            //Nethereum.
-
-            var transactionPolling = web3.TransactionManager.TransactionReceiptService;
-
-            //var currentBalance = await web3.Eth.GetBalance.SendRequestAsync(account.Address);
-
-            //assumed client is mining already
-            //when sending a transaction using an Account, a raw transaction is signed and send using the private key
-            return await transactionPolling.SendRequestAndWaitForReceiptAsync(() =>
+            var transactionInput = new TransactionInput()
             {
-                var transactionInput = new TransactionInput
-                {
-                    From = account.Address,
-                    //Gas = new HexBigInteger(25000),
-                    GasPrice = new HexBigInteger(10 ^ 10),
-                    To = recipient.Address,
-                    Value = new HexBigInteger(new BigInteger(recipient.Value)),
-                    Nonce = web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(account.Address).Result
-                };
-                var txSigned = new Nethereum.Signer.TransactionSigner();
-                var signedTx = txSigned.SignTransaction(account.PrivateKey, transactionInput.To, transactionInput.Value, transactionInput.Nonce);
-                var transaction = new Nethereum.RPC.Eth.Transactions.EthSendRawTransaction(web3.Client);
-                return transaction.SendRequestAsync(signedTx);
-            });
+                From = account.Address,
+                GasPrice = new HexBigInteger(Web3.Convert.ToWei(1.5, UnitConversion.EthUnit.Gwei)),
+                To = recipient.Address,
+                Value = new HexBigInteger(new BigInteger(recipient.Value))
+            };
+
+            return web3.Eth.TransactionManager.SendTransactionAsync(transactionInput);
+            
         }
 
-        private static Web3 GetConnection()
+        private static Web3 GetConnection(Account account)
         {
-            return new Web3("https://mainnet.infura.io");
+            return new Web3(account, "https://mainnet.infura.io");
         }
 
         private static Account SetupAccount()
---
+++ AutomatedEtherTransfer_Windows/Program.cs
@@ -0,0 +1,71 @@
+﻿using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nethereum.HdWallet;
+using Nethereum.Util;
+using Nethereum.Web3.Accounts.Managed;
+
+namespace AutomatedEtherTransfer_Windows
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //MainSend();
+            Task.Run(async () => await TestSend()).GetAwaiter().GetResult();
+
+        }
+
+        private static async Task TestSend()
+        {
+            var account = SetupTestAccount();
+            var recipients = new Wallet("blue inherit drum enroll amused please camp false estate flash sell right", null).GetAddresses();
+            var transanctionHashes = new List<string>();
+
+            //var recipients = new List<Records>() { new Records() { Value = 10000000000000000, Address = "0x5CC494843e3f4AC175A5e730c300b011FAbF2cEa" } };
+            foreach (var recipient in recipients)
+            {
+                try
+                {
+                    var web3 = GetTestConnection(account);
+                    var balance = await web3.Eth.GetBalance.SendRequestAsync(recipients[0]);
+                    var transactionHash = await SendTestEther(account, recipient, web3);
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("Failed");
+                }
+            }
+        }
+
+        private static Task<string> SendTestEther(Account account, string recipient, Web3 web3)
+        {
+            var transactionInput = new TransactionInput()
+            {
+                From = account.Address,
+                GasPrice = new HexBigInteger(Web3.Convert.ToWei(1.5, UnitConversion.EthUnit.Gwei)),
+                To = recipient,
+                Value = new HexBigInteger(new BigInteger(0.1))
+            };
+
+            return web3.Eth.TransactionManager.SendTransactionAsync(transactionInput);
+
+        }
+
+        private static Web3 GetTestConnection(Account account)
+        {
+            return new Web3(account, "https://rinkeby.infura.io/v3/dc22c9c6245742069d5fe663bfa8a698");
+        }
+
+        private static Account SetupTestAccount() => new Wallet("blue inherit drum enroll amused please camp false estate flash sell right", null).GetAccount(0);
+    }
+}
